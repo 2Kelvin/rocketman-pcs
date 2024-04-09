075d4363def33a5ddc572979576c5468f6bbebfa
2Kelvin <rocketmancodes02@gmail.com>
2024-04-09 08:46:08 +0300

    calculate product discount + tweak styling

--- src/components/Product/ProductCard.js
+++ src/components/Product/ProductCard.js
@@ -19,9 +19,11 @@ export default function ProductCard({ product }) {
                 product.newPrice
                     ? <p className='price' style={{ marginBottom: '0.3rem' }}>
                         <span style={{ opacity: 0.3, textDecoration: 'line-through' }}>Ksh {product.price}</span><span className='savePrice'>
-                            Save { // converting price to numbers & calculating the discount (rounded up)
-                                (((product.price - product.newPrice) / product.price) * 100)
-                            }%
+                            { // converting price to numbers & calculating the discount (rounded up)
+                                Math.round(
+                                    ((parseInt(product.price, 10) - parseInt(product.newPrice, 10)) / parseInt(product.price, 10)) * 100
+                                )
+                            }% off
                         </span>
                     </p>
                     : <p className='price'>Ksh {product.price}</p>
