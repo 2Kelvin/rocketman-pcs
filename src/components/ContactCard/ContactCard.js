4e4527155920e3695a4071f5d8b97ea3ef589a34
2Kelvin <rocketmancodes02@gmail.com>
2024-04-10 10:41:35 +0300

    add + style contact card popup

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -1,15 +1,28 @@
-import { IoCloseOutline } from "react-icons/io5";
+import { IoCloseOutline, IoCallOutline } from "react-icons/io5";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { PiWhatsappLogoLight } from "react-icons/pi";
 import './ContactCard.css';
 
 
-export default function ContactCard() {
+export default function ContactCard({ showContact, setShowContact }) {
     return (
         <div className='contactCard'>
             <div className='flexWrapper'>
-                <div className='closeIconDiv'>
+                <div className='closeIconDiv' onClick={() => setShowContact(false)}>
                     <IoCloseOutline className='closeIcon' />
                 </div>
             </div>
+
+            <p>Call, text or whatsapp me using the number below.</p>
+
+            <div className='iconsContactDiv'>
+                <IoCallOutline className='iconContactBuyer phoneIcon' />
+                <BiMessageSquareDetail className='iconContactBuyer msgIcon' />
+                <PiWhatsappLogoLight className='iconContactBuyer whatsappIcon' />
+            </div>
+
+            <h3>0772775311</h3>
+            <p>Thank you for visting my shop.</p>
         </div>
     );
 }
