39ce757ee5b0fd5e1e430281177c717a843c6057
2Kelvin <rocketmancodes02@gmail.com>
2024-04-12 10:11:15 +0300

    done styling morepics component

--- src/components/MorePics/MorePics.js
+++ src/components/MorePics/MorePics.js
@@ -7,22 +7,24 @@ import pic from "./../../images/570.jpg";
 export default function MorePics({ setShowMorePics, allImages, imageAlt }) {
     return (
         <section className="morePicsSection">
-                <div className="iconCloseWrapper">
+            <div className="iconCloseWrapper">
+                <div className="closeDivIcon">
                     <IoCloseOutline
                         className="closeGalleryIcon"
                         onClick={() => setShowMorePics(false)}
                     />
                 </div>
+            </div>
 
             <div className="imagesDirWrapper">
-                <div className="dirIconrDiv"><GrLinkPrevious className="dirIcon" /></div>
+                <button className="dirIconBtn"><GrLinkPrevious className="dirIcon" /></button>
 
                 <div className="imagesDiv">
                     <img src={pic} alt="pic" />
                     <p>1 / 2</p>
                 </div>
 
-                <div className="dirIconrDiv"><GrLinkNext className="dirIcon" /></div>
+                <button className="dirIconBtn"><GrLinkNext className="dirIcon" /></button>
             </div>
         </section>
     );
