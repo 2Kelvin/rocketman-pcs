96ba386f9818c6a22c5c311b695e853f665b3cbe
2Kelvin <rocketmancodes02@gmail.com>
2024-04-12 12:11:41 +0300

    make contact card responsive

--- src/components/OneImage/OneImage.js
+++ src/components/OneImage/OneImage.js
@@ -1,10 +1,7 @@
-import "./OneImage.css";
-
-
 export default function OneImage({ onePicture, imageAlt, imgIndex, imagesCount }) {
     return (
         <>
-            <img src={onePicture} alt={imageAlt} loading="lazy"/>
+            <img src={onePicture} alt={imageAlt} loading="lazy" />
             <p>{imgIndex + 1} of {imagesCount}</p>
         </>
     );
