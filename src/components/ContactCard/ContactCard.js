d751f74d6cc7f2712c90d869e2701ed34c92b4bb
2Kelvin <rocketmancodes02@gmail.com>
2024-04-13 16:18:06 +0300

    add blurred background to ContactCard component

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -6,23 +6,25 @@ import './ContactCard.css';
 
 export default function ContactCard({ showContact, setShowContact }) {
     return (
-        <div className='contactCard'>
-            <div className='flexWrapper'>
-                <div className='closeIconDiv' onClick={() => setShowContact(false)}>
-                    <IoCloseOutline className='closeIcon' />
+        <div className="contactCardBlurredBg">
+            <div className='contactCard'>
+                <div className='flexWrapper'>
+                    <div className='closeIconDiv' onClick={() => setShowContact(false)}>
+                        <IoCloseOutline className='closeIcon' />
+                    </div>
                 </div>
+
+                <p>Call, text or whatsapp me using the number below.</p>
+
+                <div className='iconsContactDiv'>
+                    <IoCallOutline className='iconContactBuyer phoneIcon' />
+                    <BiMessageSquareDetail className='iconContactBuyer msgIcon' />
+                    <PiWhatsappLogoLight className='iconContactBuyer whatsappIcon' />
+                </div>
+
+                <h3>0772775311</h3>
+                <p>Thank you for visting my shop.</p>
             </div>
-
-            <p>Call, text or whatsapp me using the number below.</p>
-
-            <div className='iconsContactDiv'>
-                <IoCallOutline className='iconContactBuyer phoneIcon' />
-                <BiMessageSquareDetail className='iconContactBuyer msgIcon' />
-                <PiWhatsappLogoLight className='iconContactBuyer whatsappIcon' />
-            </div>
-
-            <h3>0772775311</h3>
-            <p>Thank you for visting my shop.</p>
         </div>
     );
 }
