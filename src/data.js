47dae562b465f1d656e85589fe8e88f211806f71
2Kelvin <rocketmancodes02@gmail.com>
2024-05-13 13:55:11 +0300

    add 1050ti data

--- src/data.js
+++ src/data.js
@@ -30,6 +30,14 @@ import gtx970_1 from './images/gpus/zotac_970/gtx_970_1.png';
 import gtx970_3 from './images/gpus/zotac_970/gtx_970_3.png';
 import gtx970_5 from './images/gpus/zotac_970/gtx_970_5.jpg';
 
+// zotac 1050ti
+import gtx1050ti_1 from './images/gpus/gtx1050ti/gtx1050ti1.jpg';
+import gtx1050ti_3 from './images/gpus/gtx1050ti/gtx1050ti3.jpg';
+import gtx1050ti_4 from './images/gpus/gtx1050ti/gtx1050ti4.jpg';
+import gtx1050ti_5 from './images/gpus/gtx1050ti/gtx1050ti5.jpg';
+import gtx1050ti_6 from './images/gpus/gtx1050ti/gtx1050ti6.jpg';
+import gtx1050ti_7 from './images/gpus/gtx1050ti/gtx1050ti7.jpg';
+
 const rx570 = {
     id: 1,
     image: img570,
@@ -144,6 +152,25 @@ const gtx970_gpu = {
     allImages: [gtx970_1, gtx970_3, gtx970_5],
 };
 
+const gtx1050ti = {
+    id: 7,
+    image: gtx1050ti_1,
+    alt: 'EVGA GTX 1050Ti 4GB',
+    name: 'EVGA GTX 1050Ti 4GB',
+    type: 'Graphics Card',
+    specs: [
+        '4GB memory size',
+        'DDR5 memory type',
+        '256 bit memory bus',
+        '1216MHz core clock',
+        'PCIE 3.0 x 16 bus interface',
+    ],
+    price: '18,800',
+    newPrice: '18,500',
+    sold: true,
+    allImages: [gtx1050ti_3, gtx1050ti_4, gtx1050ti_5, gtx1050ti_6, gtx1050ti_7],
+};
+
 export const myStock = [
     rx570,
     msi480_4gb,
@@ -151,4 +178,5 @@ export const myStock = [
     msi580_8gb,
     xfx580_4gb,
     gtx970_gpu,
+    gtx1050ti,
 ];
